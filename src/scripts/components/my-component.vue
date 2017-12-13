d53aa259ae0e09d9f8d76dac0c8df939081dbdf2
Shigeki Suwa <suwa.shigeki@gmail.com>
2017-12-13 19:18:29 +0900

    postcss and posthtml

---
+++ src/scripts/components/my-component.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  div {{hoge}}
+</template>
+
+<script>
+export default {
+  data: () => ({
+    hoge: 'helllo',
+  }),
+}
+</script>
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,13 @@
+// import Vue from 'vue';
 import MyModule from './my-module';
+// import MyComponent from './components/my-component.vue';
 
 new MyModule();
+
+// const app = new Vue({
+//   el: '#vue-app-root',
+//   template: '<my-component></my-component>',
+//   components: {
+//     MyComponent,
+//   },
+// });
